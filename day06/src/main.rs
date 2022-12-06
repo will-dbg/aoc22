6c981eb87f9f7c3fbe901e6be267f0b5249f7046
WilliamBoulton <will@willboulton.com>
2022-12-06 18:38:09 +0000

    day 06

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,23 +1,34 @@
+use std::collections::HashSet;
 use std::io::{BufRead, BufReader};
 use std::fs::File;
-use itertools::structs::Unique;
+use std::collections::VecDeque;
 
 fn main() {
     let lines = BufReader::new(File::open("input.txt").expect("open failed"))
     .lines().map(|l|l.unwrap()).collect::<Vec<String>>();
     let line = &lines[0].chars().collect::<Vec<char>>();
-    let mut window : Vec<char> = Vec::new();
+    println!("part1: {}",find_unique_range(line, 4));
+    println!("part2: {}",find_unique_range(line, 14));
+}
 
-    for i in (0..line.len()){
+fn find_unique_range(line : &Vec<char>, of_size : usize) -> usize{
+    let mut window : VecDeque<char> = VecDeque::new();
+
+    for i in  0..line.len(){
         let ch = line[i];
-        if window.len() < 4 {
-            window.push(ch);
+        if window.len() < of_size {
+            window.push_back(ch);
         }else{
-            window.dedup();
-            if window.len() == 4 {
-                println!("Part1: {}",i);
-                break;
+            let mut iniq = HashSet::new();
+            window.iter().all( |x| iniq.insert(x));
+            if iniq.len() == of_size {
+                return i;
+            }
+            else{
+                window.pop_front();
+                window.push_back(ch);
             }
         }
     }
+    panic!()
 }
