cb4beff28a9f47b79e35ffafd3f194b57b703dd2
WilliamBoulton <will@willboulton.com>
2022-12-06 18:51:32 +0000

    oneliner

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -8,28 +8,21 @@ fn main() {
         .lines()
         .map(|l|l.unwrap())
         .collect::<Vec<String>>();
+
+
+
     let line = &lines[0].chars().collect::<Vec<char>>();
+
+    
+
+
     println!("part1: {}",find_unique_range(line, 4));
     println!("part2: {}",find_unique_range(line, 14));
 }
 
-fn find_unique_range(line : &Vec<char>, of_size : usize) -> usize{
-    let mut window : VecDeque<char> = VecDeque::new();
 
-    for i in  0..line.len(){
-        let ch = line[i];
-        if window.len() < of_size {
-            window.push_back(ch);
-        }else{
-            let mut iniq = HashSet::new();
-            if  window.iter().all( |x| iniq.insert(x)) {
-                return i;
-            }
-            else{
-                window.pop_front();
-                window.push_back(ch);
-            }
-        }
-    }
-    panic!()
+fn find_unique_range(line : &Vec<char>, of_size : usize) -> usize{
+    return of_size + line
+        .windows(of_size)
+        .position(|window| !(0..(of_size-1)).any(|c| (c+ 1..of_size).any(|d| window[c]==window[d]))).unwrap();
 }
